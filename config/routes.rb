22aa2ffb016878d7c877d01abfeb4dca3a43a90a
rajesh23456 <95840507+rajesh23456@users.noreply.github.com>
2022-08-24 17:01:02 +0530

    second-commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root 'sessions#home'
 
+  resources :users, only: [:new, :create, :edit, :update, :show, :destroy]
   # Defines the root path route ("/")
   # root "articles#index"
+  get '/login', to: 'sessions#login'
+  post '/login', to: 'sessions#create'
+  post '/logout', to: 'sessions#destroy'
+  get '/logout', to: 'sessions#destroy'
 end
